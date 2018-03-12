8b9520d96f485ae0088f6c8533facb6462e70e3c
Halvard Yri Adriaenssens <halvard@adriaenssens.no>
2018-03-12 16:09:53 +0100

    Leke med IMU'en
    Diverse programmet til IMU

---
+++ Testing/Testing.ino
@@ -0,0 +1,28 @@
+
+
+void setup() 
+{
+  Serial.begin(115200);
+  
+  float verdi1;
+  float verdi2;
+  float verdi3;
+  
+  verdi1 = 5.12;
+  verdi2 = 7.98;
+  verdi3 = 2.62;
+
+  float arr[3] = {
+    verdi1, verdi2, verdi3
+  };
+
+for(int i = 0; i < 3; i++)
+  {
+    Serial.println(arr[i]);
+  }
+}
+
+void loop() 
+{
+  
+}
