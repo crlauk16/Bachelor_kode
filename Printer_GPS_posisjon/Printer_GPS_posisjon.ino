62d458432bd021ebe1641503e96f0f49a45c2332
Halvard Yri Adriaenssens <halvard@adriaenssens.no>
2018-05-09 09:02:03 +0200

    La til program for GPS, både simulnk og arduino
    La også til strøm og spenningsmåler

--- Printer_GPS_posisjon/Printer_GPS_posisjon.ino
+++ Printer_GPS_posisjon/Printer_GPS_posisjon.ino
@@ -1,30 +1,25 @@
 #include <TinyGPS++.h> // NMEA parsing: http://arduiniana.org
+#include <Wire.h>
 
-#define ArduinoSerial Serial1
 #define GPSSerial Serial2
 #define GPSBaud 9600
-
-TinyGPSPlus tinygps;
-
 #define BEACON_INTERVAL 3
 
-int byteRead;
+TinyGPSPlus tinygps;
 
 void setup() {                
 // Turn the Serial Protocol ON
   Serial.begin(9600);
-  ArduinoSerial.begin(9600);
   while (!Serial);
   GPSSerial.begin(GPSBaud);
+  
+  Wire.begin(8);                // join i2c bus with address #8 
+  Wire.onRequest(requestEvent); // register event
 }
 
 void loop() {
-  
   unsigned long loopStartTime = millis();
-
-  // Begin listening to the GPS
-  Serial.println("Beginning to listen for GPS traffic...");
-
+  
   // Look for GPS signal for up to 7 minutes
   while ((!tinygps.location.isValid() || !tinygps.date.isValid()) && 
     millis() - loopStartTime < 7UL * 60UL * 1000UL)
@@ -36,52 +31,67 @@ void loop() {
   // Did we get a GPS fix?
   if (!tinygps.location.isValid())
   {
-    Serial.println(F("Could not get GPS fix."));
-    Serial.print(F("GPS characters seen = "));
-    Serial.println(tinygps.charsProcessed());
-    Serial.print(F("Checksum errors = "));
-    Serial.println(tinygps.failedChecksum());
     return;
   }
 
-  Serial.println(F("A GPS fix was found!"));
-
-  // Step 3: Start talking to the RockBLOCK and power it up
-  Serial.println("Beginning to talk to the RockBLOCK...");
-
-  char outBufferLat[24];
-  sprintf(outBufferLat, "%s%u.%09lu",
+  char outBufferLat[12];
+  sprintf(outBufferLat, "%s%u%09lu",
 
   tinygps.location.rawLat().negative ? "-" : "",
   tinygps.location.rawLat().deg,
   tinygps.location.rawLat().billionths);
 
-  char outBufferLng[24];
-  sprintf(outBufferLng, "%s%u.%09lu",
+  char outBufferLng[12];
+  sprintf(outBufferLng, "%s%u%09lu",
 
   tinygps.location.rawLng().negative ? "-" : "",
   tinygps.location.rawLng().deg,
   tinygps.location.rawLng().billionths);
-  
-  Serial.print(outBufferLat);
-  Serial.print(", ");
-  Serial.println(outBufferLng);
 
-  ArduinoSerial.print(outBufferLat);
-  //ArduinoSerial.print(", ");
-  //ArduinoSerial.println(outBufferLat);
-  
+  //Transfer outBufferLat in to int
+  int16_t latHel = outBufferLat[0] - 48;
+  for (int i = 1; i < 3; i++)
+  {
+     latHel = latHel * 10 + outBufferLat[i] - 48;
+  }
+  int16_t latDec = outBufferLat[3] - 48;
+  for (int i = 4; i < 7; i++)
+  {
+     latDec = latDec * 10 + outBufferLat[i] - 48;
+  }
 
+  //Transfer outBufferLng in to int
+  int16_t lngHel = outBufferLng[0] - 48;
+  for (int i = 1; i < 3; i++)
+  {
+     lngHel = lngHel * 10 + outBufferLng[i] - 48;
+  }
+  int16_t lngDec = outBufferLng[3] - 48;
+  for (int i = 4; i < 7; i++)
+  {
+     lngDec = lngDec * 10 + outBufferLng[i] - 48;
+  }
+  Serial.print(latHel);
+  Serial.println(latDec);
+  Serial.print(lngHel);
+  Serial.println(lngDec); 
+
+  // Sleep
   int elapsedSeconds = (int)((millis() - loopStartTime) / 1000);
   if (elapsedSeconds < BEACON_INTERVAL)
   {
     int delaySeconds = BEACON_INTERVAL - elapsedSeconds;
     Serial.print(F("Waiting for "));
-    Serial.print(delaySeconds);
+    Serial.println(delaySeconds);
     Serial.println(F(" seconds"));
     delay(1000UL * delaySeconds);
   }
 
   // Wake
   Serial.println("Wake up!");
+}
+
+void requestEvent() 
+{
+  Wire.write("hello ");
 }
